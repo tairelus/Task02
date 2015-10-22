fb996f40e673d405f5f11261d3a38dc707223c88
Yuriy Mykytenko <yuriy.mykytenko@transwareag.com>
2015-10-22 15:27:19 +0300

    Task02: Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//If class declared as public file name must be the same as class name
+//Java code usually written in K&R "One True Brace Style" (1TBS) style.
+//I prefer Eric Allman style because it allows easily to find '{' character 
+//especially with modern development environments
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+}
